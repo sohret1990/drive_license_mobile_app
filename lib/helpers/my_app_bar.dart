09c621a8939510835352bcc57883ddecb8355b1e
sohret.suleymanov@gmail.com <sohret1140>
2023-02-03 18:13:50 +0400

    test

--- lib/helpers/my_app_bar.dart
+++ lib/helpers/my_app_bar.dart
@@ -2,16 +2,30 @@ import 'package:flutter/material.dart';
 
 class MyAppBar extends AppBar{
 
-  final String caption;
-  bool isCenter = true;
+  String caption;
+  IconData? iconData;
+  bool? isCenter = true;
 
-  MyAppBar({required this.caption, isCenter});
+  MyAppBar({this.iconData, this.isCenter, required this.caption}) {
 
-  @override 
-  // TODO: implement title
-  Widget? get title => Text(caption);
+    this.caption = caption;
+    this.isCenter = isCenter;
+    this.iconData = iconData;
 
-  @override
-  // TODO: implement centerTitle
-  bool? get centerTitle => isCenter;
+  }
+
+@override
+// TODO: implement title
+  Widget? get title => this.iconData != null? Container(
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Icon(this.iconData),
+        Text(caption),
+      ],
+    ),
+  ):Text(caption);
+
+
 }
