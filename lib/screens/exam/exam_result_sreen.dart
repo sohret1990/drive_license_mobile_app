0843db30660b3a5d1107985e69d87fac770f9245
sohret.suleymanov@gmail.com <sohret1140>
2023-04-27 18:36:22 +0400

    test

--- lib/screens/exam/exam_result_sreen.dart
+++ lib/screens/exam/exam_result_sreen.dart
@@ -3,11 +3,10 @@ import 'package:drive_license_app/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class ExamResultScreen extends StatefulWidget {
-  const ExamResultScreen(
-      {Key? key,
-      required this.isSuccessed,
-      required this.questionList,
-      required this.answersList})
+  const ExamResultScreen({Key? key,
+    required this.isSuccessed,
+    required this.questionList,
+    required this.answersList})
       : super(key: key);
   final bool isSuccessed;
   final List<QuestionModel> questionList;
@@ -20,23 +19,85 @@ class ExamResultScreen extends StatefulWidget {
 class _ExamResultScreenState extends State<ExamResultScreen> {
   @override
   Widget build(BuildContext context) {
+    var windowWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    var windowHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
     return Scaffold(
       appBar: MyAppBar(
-        iconData: Icons.receipt_sharp,
+        iconData: null, // Icons.receipt_sharp,
         isCenter: false,
         caption: "",
       ),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Image(image: AssetImage("assets/images/drive_license.png"),
-            fit: BoxFit.fitWidth,
-
+      backgroundColor: Colors.indigoAccent,
+      body: Container(
+        alignment: Alignment.center,
+        child: Container(
+          width: windowWidth - 80,
+          height: windowHeight / 1.5,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.white,
+                  blurStyle: BlurStyle.solid,
+                  blurRadius: 20),
+              BoxShadow(
+                  color: Colors.black26,
+                  blurStyle: BlurStyle.solid,
+                  blurRadius: 20),
+              BoxShadow(
+                  color: Colors.white,
+                  blurStyle: BlurStyle.solid,
+                  blurRadius: 20),
+              BoxShadow(
+                  color: Colors.black26,
+                  blurStyle: BlurStyle.solid,
+                  blurRadius: 20),
+            ],
+          ),
+          alignment: Alignment.center,
+          //height: 80,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                Text("Təbriklər qazandınız!", style: TextStyle(fontSize: 20)),
+            SizedBox(
+              height: 20,
             ),
-          )
+            Image(
+              image: AssetImage("assets/images/cup.png"),
+              height: windowHeight / 6,
+            ),
+            /*SizedBox.fromSize(size: Size.fromHeight(20)),
+                  Text("Cəmi: 10", style: TextStyle(fontSize: 20)),
+                  SizedBox.fromSize(size: Size.fromHeight(5)),
+                  Text("Düzgün cavab: 3", style: TextStyle(fontSize: 20)),
+                  SizedBox.fromSize(size: Size.fromHeight(5)),
+                  Text("Səhv cavab: 7", style: TextStyle(fontSize: 20)),
+                  SizedBox.fromSize(size: Size.fromHeight(5)),*/
+
+            /*List<int>.generate(2, (1) => Image(
+            image: AssetImage(
+            "assets/images/azpdd/button${index + 1}.jpg",
+            ),
+            height: 10,
+            width: 10,
+          ),
+        ),*/
         ],
-      )
+      ),
+    ),)
+    ,
+    )
     );
   }
 }
