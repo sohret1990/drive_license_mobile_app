91e4592ba2117df57b56f45ea73d6472d52918b8
sohret.suleymanov@gmail.com <sohret1140>
2023-04-28 17:35:26 +0400

    test

--- lib/screens/exam/exam_result_sreen.dart
+++ lib/screens/exam/exam_result_sreen.dart
@@ -3,10 +3,11 @@ import 'package:drive_license_app/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class ExamResultScreen extends StatefulWidget {
-  const ExamResultScreen({Key? key,
-    required this.isSuccessed,
-    required this.questionList,
-    required this.answersList})
+  const ExamResultScreen(
+      {Key? key,
+      required this.isSuccessed,
+      required this.questionList,
+      required this.answersList})
       : super(key: key);
   final bool isSuccessed;
   final List<QuestionModel> questionList;
@@ -19,14 +20,8 @@ class ExamResultScreen extends StatefulWidget {
 class _ExamResultScreenState extends State<ExamResultScreen> {
   @override
   Widget build(BuildContext context) {
-    var windowWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    var windowHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
+    var windowWidth = MediaQuery.of(context).size.width;
+    var windowHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: MyAppBar(
         iconData: null, // Icons.receipt_sharp,
@@ -64,40 +59,67 @@ class _ExamResultScreenState extends State<ExamResultScreen> {
           alignment: Alignment.center,
           //height: 80,
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(10.0),
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                Text("Təbriklər qazandınız!", style: TextStyle(fontSize: 20)),
-            SizedBox(
-              height: 20,
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(
+                    this.widget.isSuccessed
+                        ? "Təbriklər qazandınız!"
+                        : "Təəssüf keçmədiniz!",
+                    style: TextStyle(fontSize: 20)),
+                SizedBox(
+                  height: 20,
+                ),
+                Image(
+                  image: AssetImage(
+                      "assets/images/${this.widget.isSuccessed ? "cup" : "game_over"}.png"),
+                  height: windowHeight / 6,
+                ),
+                SizedBox.fromSize(size: Size.fromHeight(20)),
+                Text("Cəmi: 10", style: TextStyle(fontSize: 20)),
+                SizedBox.fromSize(size: Size.fromHeight(5)),
+                Text("Düzgün cavab: ${getCorrectAnswerCount()}", style: TextStyle(fontSize: 20)),
+                SizedBox.fromSize(size: Size.fromHeight(5)),
+                Text("Səhv cavab: ${getWrongAnswerCount()}", style: TextStyle(fontSize: 20)),
+                SizedBox.fromSize(size: Size.fromHeight(5)),
+                getQuestionPanel(),
+              ],
             ),
-            Image(
-              image: AssetImage("assets/images/cup.png"),
-              height: windowHeight / 6,
-            ),
-            /*SizedBox.fromSize(size: Size.fromHeight(20)),
-                  Text("Cəmi: 10", style: TextStyle(fontSize: 20)),
-                  SizedBox.fromSize(size: Size.fromHeight(5)),
-                  Text("Düzgün cavab: 3", style: TextStyle(fontSize: 20)),
-                  SizedBox.fromSize(size: Size.fromHeight(5)),
-                  Text("Səhv cavab: 7", style: TextStyle(fontSize: 20)),
-                  SizedBox.fromSize(size: Size.fromHeight(5)),*/
-
-            /*List<int>.generate(2, (1) => Image(
-            image: AssetImage(
-            "assets/images/azpdd/button${index + 1}.jpg",
-            ),
-            height: 10,
-            width: 10,
           ),
-        ),*/
-        ],
+        ),
       ),
-    ),)
-    ,
-    )
     );
+  }
+
+  Widget getQuestionPanel() {
+    return Expanded(
+      flex: 1,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: this.widget.questionList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Image(
+                width: 20,
+                height: 20,
+                image: AssetImage(
+                    "assets/images/azpdd/button${(index % 2 == 0) ? "${index + 1}selected" : index + 1}.jpg"),
+              ),
+            );
+          }),
+    );
+  }
+
+  int getCorrectAnswerCount(){
+    var questions =this.widget.questionList;
+    var answers =this.widget.answersList;
+    return questions.where((q) => q.correctAnswer.answerNo == answers.entries ).length;
+  }
+
+  int getWrongAnswerCount(){
+    return this.widget.questionList.where((element) => element.correctAnswer.answerNo == 1).length;
   }
 }
